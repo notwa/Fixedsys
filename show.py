b4d54224c3b56990992a8caee37e51d5b842860d
Connor Olding <cloningdonor@gmail.com>
2024-04-23 04:40:24 -0700

    turn procedure into a function

--- show.py
+++ show.py
@@ -2,45 +2,48 @@ from PIL import Image
 import re
 import sys
 
-filename = sys.argv[1]
 
-f = open(filename)
-for i in range(5):
-    f.readline()
-height = int(re.match("height (\d+)", f.readline()).group(1))
-for i in range(10):
-    f.readline()
+def show(filename):
+    f = open(filename)
+    for i in range(5):
+        f.readline()
+    height = int(re.match("height (\d+)", f.readline()).group(1))
+    for i in range(10):
+        f.readline()
 
-chars = []
-rows = 14
-cols = 16
-max_width = 0
-for i in range(256):
-    assert f.readline() == "char %d\n" % i
-    width = int(re.match("width (\d+)", f.readline()).group(1))
-    if i < 32 and width:
-        rows = 16
-    max_width = max(max_width, width)
-    char = Image.new("1", (width, height), 1)
-    if width:
-        pix = char.load()
-        for y in range(height):
-            line = f.readline()
-            for x in range(width):
-                pix[x, y] = 1 - int(line[x])
-    chars.append(char)
-    f.readline()
+    chars = []
+    rows = 14
+    cols = 16
+    max_width = 0
+    for i in range(256):
+        assert f.readline() == "char %d\n" % i
+        width = int(re.match("width (\d+)", f.readline()).group(1))
+        if i < 32 and width:
+            rows = 16
+        max_width = max(max_width, width)
+        char = Image.new("1", (width, height), 1)
+        if width:
+            pix = char.load()
+            for y in range(height):
+                line = f.readline()
+                for x in range(width):
+                    pix[x, y] = 1 - int(line[x])
+        chars.append(char)
+        f.readline()
 
-im = Image.new("1", ((max_width + 1) * cols + 1, (height + 1) * rows + 1), 0)
-skip = 16 - rows
-for i in range(16 * skip, 256):
-    x = i % 16 * (max_width + 1) + 1
-    y = (i // 16 - skip) * (height + 1) + 1
-    char = Image.new("1", (max_width, height), 1)
-    char.paste(chars[i], (0, 0))
-    im.paste(char, (x, y))
+    im = Image.new("1", ((max_width + 1) * cols + 1, (height + 1) * rows + 1), 0)
+    skip = 16 - rows
+    for i in range(16 * skip, 256):
+        x = i % 16 * (max_width + 1) + 1
+        y = (i // 16 - skip) * (height + 1) + 1
+        char = Image.new("1", (max_width, height), 1)
+        char.paste(chars[i], (0, 0))
+        im.paste(char, (x, y))
 
-if filename.endswith(".fd"):
-    filename = filename[:-3]
+    if filename.endswith(".fd"):
+        filename = filename[:-3]
 
-im.save(filename + ".png")
+    im.save(filename + ".png")
+
+
+show(sys.argv[1])
