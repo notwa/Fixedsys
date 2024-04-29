b502f551e6306679ab7bced7301c6ce5ead7d73d
Connor Olding <cloningdonor@gmail.com>
2024-04-29 15:10:34 -0700

    add/adjust hashbangs

--- dewinfont.py
+++ dewinfont.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # dewinfont is copyright 2001 Simon Tatham. All rights reserved.
 #
--- show.py
+++ show.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from PIL import Image
 from dataclasses import dataclass, field
 
